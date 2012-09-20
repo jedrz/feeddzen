1f2c92b7db7db9419d0d5b627d616c4a884b52eb
Łukasz Jędrzejewski <lukasz892@gmail.com>
2012-09-20 16:22:58 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='feeddzen',
+    description='Python wrapper around dzen2',
+    author='Łukasz Jędrzejewski',
+    author_email='jedrzejewskiluk@gmail.com',
+    packages=['feeddzen', 'feeddzen.plugins']
+)
