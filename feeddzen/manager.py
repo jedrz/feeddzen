8ca38390dfea968cf9594e1c40bc04696b96f5d7
Łukasz Jędrzejewski <lukasz892@gmail.com>
2012-10-07 12:59:06 +0200

    Fix manager class docs to be compatible with sphinx

--- feeddzen/manager.py
+++ feeddzen/manager.py
@@ -10,20 +10,20 @@ from .plugins.core import StaticWidget
 
 
 class Manager:
-    """Manager for plugins and dzen process"""
+    """Manager for plugins and dzen process.
+
+    :param widgets: a list of widgets to display in dzen
+    :param dzen_command: a command to invoke dzen
+    """
 
     def __init__(self, widgets=[], dzen_command='dzen2'):
-        """Arguments:
-        - `widgets` - a list of widgets
-        - `dzen_command` - a command to run dzen, by default 'dzen2'
-        """
         self._scheduler = utils.ContScheduler(time.time, time.sleep)
         self.widgets = widgets
         self._init_dzen(dzen_command)
         self._init_events()
 
     def _init_dzen(self, dzen_command):
-        """Create dzen process and set up its standard input"""
+        """Create dzen process and set up its standard input."""
         dzen_proc = subprocess.Popen(shlex.split(dzen_command),
                                      stdin=subprocess.PIPE)
         self._dzen_stdin = dzen_proc.stdin
@@ -46,11 +46,11 @@ class Manager:
                     widget.timeout, 1, self._print_status_bar, ())
 
     def _print_status_bar(self):
-        """Send all widgets' output to dzen's stdin"""
+        """Send all widgets' output to dzen's stdin."""
         status_bar = ''.join(str(w) for w in self.widgets) + '\n'
         self._dzen_stdin.write(bytes(status_bar.encode('utf-8')))
 
     def start(self):
-        """Run scheduler and start sending data to dzen"""
+        """Run scheduler and start sending data to dzen."""
         self._print_status_bar()
         self._scheduler.run()
